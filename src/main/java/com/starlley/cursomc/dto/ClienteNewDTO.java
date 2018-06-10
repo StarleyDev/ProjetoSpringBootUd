542db8f83312f7f3c5d629413917270c05844bba
Starley Cazorla <stdev@outlook.com.br>
2018-06-10 10:37:38 -0300

    Validation customized with CPF or CNPJ

--- src/main/java/com/starlley/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/starlley/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,48 @@ package com.starlley.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.starlley.cursomc.services.validation.ClienteInsert;
+
+// Anotação ClienteInsert e para realizar a validação de campos //
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	// Validações sintaticas //
+	@NotEmpty(message = "Preenchimento obrigatorio!")
+	@Length(min = 5, max = 120, message = "O tamenho deve ser entre 5 e 120 caracteres!")
 	private String nome;
+
+	@NotEmpty(message = "Preenchimento obrigatorio!")
+	@Email(message = "Email invalido!")
 	private String email;
+
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String logradouro;
+
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String numero;
+
 	private String complemento;
 	private String bairro;
+
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String cep;
 
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String telefone1;
+
 	private String telefone2;
 	private String telefone3;
 
---
+++ src/main/java/com/starlley/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,47 @@
+package com.starlley.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.starlley.cursomc.domain.Cliente;
+import com.starlley.cursomc.domain.enums.TipoCliente;
+import com.starlley.cursomc.dto.ClienteNewDTO;
+import com.starlley.cursomc.resources.exceptions.FieldMessage;
+import com.starlley.cursomc.services.validation.utils.BR;
+
+// Classe para tratamento de erro e validação //
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		// Pessoa Fisica //
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+
+		// Pessoa Juridica //
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}

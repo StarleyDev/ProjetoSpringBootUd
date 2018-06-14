a2438a2ff6b75b124efdce67697162db1f3e7ed5
Starley Cazorla <stdev@outlook.com.br>
2018-06-14 12:31:31 -0300

    Created profile DEV

---
+++ src/main/java/com/starlley/cursomc/config/DevConfig.java
@@ -0,0 +1,41 @@
+package com.starlley.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.starlley.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	// Criando um BEAN para iniciar o bando de dados //
+
+	@Autowired
+	private DBService dbService;
+
+	// Pegando o valor da chave da aplication-dev.properties //
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDateBase() throws ParseException {
+
+		// Criando a condição "se-não" //
+		if (!"create".equals(strategy)) {
+
+			return false;
+		}
+
+		// irá criar o banco de dados //
+		dbService.instantiateTestDataBase();
+
+		return true;
+	}
+
+}
--- src/main/java/com/starlley/cursomc/services/DBService.java
+++ src/main/java/com/starlley/cursomc/services/DBService.java
@@ -63,6 +63,7 @@ public class DBService {
 		Categoria cat5 = new Categoria(null, "Jardinagem");
 		Categoria cat6 = new Categoria(null, "Decoração");
 		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Perfumaria");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -110,7 +111,7 @@ public class DBService {
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		// Objeto resposavel por salvar as categorias e produtos //
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 
 		// Salvando os estados e cidades //
